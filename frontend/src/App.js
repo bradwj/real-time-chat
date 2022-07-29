5e21bcf0ba82d12fc233c2bcc0db2e76d351dc04
Brad Johnson <2021bradley@gmail.com>
2022-07-29 01:03:59 -0500

    Connection to websocket; send message; add header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { connect, sendMsg } from './api';
+import Header from "./components/Header/Header"
 
 function App() {
+  connect();
+  
+  function sendMessage() {
+    console.log("Hello");
+    sendMsg("Hello");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   );
 }
